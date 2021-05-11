bfb1869546d5dbbe17e2bdf6e23266ed678a8f55
Sardar Atabekov <sardarbekatabekov@gmail.com>
2021-05-11 19:19:53 +0600

    Updates

---
+++ precache-manifest.ab17ed6fbebe8835e77badd63cdda633.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40cc5565a5ba101abf0ba745ba4af72a",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "bc574a3a9b4c9ff02ba3",
+    "url": "/to-do/static/css/main.5bff57cd.chunk.css"
+  },
+  {
+    "revision": "162e5e46fb19df0acac4",
+    "url": "/to-do/static/js/2.5707a4f0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/to-do/static/js/2.5707a4f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc574a3a9b4c9ff02ba3",
+    "url": "/to-do/static/js/main.7b1f7401.chunk.js"
+  },
+  {
+    "revision": "1b12a6403e67341cafe1",
+    "url": "/to-do/static/js/runtime-main.f8df4fae.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/to-do/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/to-do/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/to-do/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/to-do/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "f19a6f3ec62ba8e9897df950d0635801",
+    "url": "/to-do/static/media/editIcon.f19a6f3e.svg"
+  },
+  {
+    "revision": "d22d92cd3982da0a355017b05d667915",
+    "url": "/to-do/static/media/green.d22d92cd.svg"
+  },
+  {
+    "revision": "0ca816248f0c348795e9e324c04b5b71",
+    "url": "/to-do/static/media/red.0ca81624.svg"
+  }
+]);
